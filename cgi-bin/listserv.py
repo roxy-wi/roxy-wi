cef38860b2132417e2c1c512b979bd9896d707cd
Aidaho12 <buffoon2001@mail.ru>
2018-01-15 12:28:27 +0600

    Update listserv.py

--- cgi-bin/listserv.py
+++ cgi-bin/listserv.py
@@ -1,7 +1,7 @@
 listhap= {
 	'haproxy1': '172.28.0.1',
-	'haproxy2': '172.28.0.2', 
+	'haproxy2': '172.28.0.2'
 	}
 list_hap_vip = {
-	'haproxy-vip': '172.28.0.3', 
+	'haproxy-vip': '172.28.0.3'
 	}
