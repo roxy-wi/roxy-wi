6448ca20e0bafacf411331006d70b9d31ef0a737
Pavel Loginov <aidaho@roxy-wi.org>
2022-06-30 10:43:57 +0300

    v6.1.0.0
    Change log: https://roxy-wi.org/changelog.py#6_1

--- app/history.py
+++ app/history.py
@@ -75,8 +75,14 @@ elif service == 'user':
 
 users = sql.select_users()
 
+try:
+    user_status, user_plan = funct.return_user_status()
+except Exception as e:
+    user_status, user_plan = 0, 0
+    funct.logging('localhost', 'Cannot get a user plan: ' + str(e), haproxywi=1)
+
 rendered_template = template.render(
     h2=1, autorefresh=0, title=title, role=role, user=user, users=users, serv=serv, service=service,
-    history=history, user_services=user_services, token=token
+    history=history, user_services=user_services, token=token, user_status=user_status, user_plan=user_plan
 )
 print(rendered_template)
