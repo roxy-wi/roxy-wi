ee4631f21492a9f2c78b5daaee9a53528aeef81f
Aidaho <aidaho@roxy-wi.org>
2024-01-18 20:00:44 +0300

    v7.1.1.0
    Changelog: https://roxy-wi.org/changelog#7.1.1

--- app/modules/tools/checker.py
+++ app/modules/tools/checker.py
@@ -5,21 +5,7 @@ import modules.tools.common as tools_common
 import modules.roxywi.common as roxywi_common
 
 
-def load_checker() -> None:
-    groups = sql.select_groups()
-    services = tools_common.get_services_status()
-    keepalived_settings = ''
-    haproxy_settings = ''
-    apache_settings = ''
-    nginx_settings = ''
-    keepalived_servers = ''
-    haproxy_servers = ''
-    apache_servers = ''
-    nginx_servers = ''
-    telegrams = ''
-    slacks = ''
-    pds = ''
-
+def load_checker() -> str:
     try:
         user_subscription = roxywi_common.return_user_status()
     except Exception as e:
@@ -31,27 +17,28 @@ def load_checker() -> None:
     except Exception:
         return redirect(url_for('login_page'))
 
-    if user_subscription['user_status']:
-        haproxy_settings = sql.select_checker_settings(1)
-        nginx_settings = sql.select_checker_settings(2)
-        keepalived_settings = sql.select_checker_settings(3)
-        apache_settings = sql.select_checker_settings(4)
-        user_group = roxywi_common.get_user_group(id=1)
-        telegrams = sql.get_user_telegram_by_group(user_group)
-        slacks = sql.get_user_slack_by_group(user_group)
-        pds = sql.get_user_pd_by_group(user_group)
-        haproxy_servers = roxywi_common.get_dick_permit(haproxy=1, only_group=1)
-        nginx_servers = roxywi_common.get_dick_permit(nginx=1, only_group=1)
-        apache_servers = roxywi_common.get_dick_permit(apache=1, only_group=1)
-        keepalived_servers = roxywi_common.get_dick_permit(keepalived=1, only_group=1)
+    kwargs = {
+        'user_subscription': user_subscription,
+        'user_params': user_params,
+        'lang': user_params['lang']
+    }
 
-    return render_template(
-        'ajax/load_telegram.html', services=services, telegrams=telegrams, pds=pds, groups=groups, slacks=slacks,
-        user_status=user_subscription['user_status'], user_plan=user_subscription['user_plan'], haproxy_servers=haproxy_servers,
-        nginx_servers=nginx_servers, apache_servers=apache_servers, keepalived_servers=keepalived_servers, haproxy_settings=haproxy_settings,
-        nginx_settings=nginx_settings, keepalived_settings=keepalived_settings, apache_settings=apache_settings,
-        role=user_params['role'], lang=user_params['lang']
-    )
+    if user_subscription['user_status']:
+        user_group = roxywi_common.get_user_group(id=1)
+        kwargs.setdefault('services', tools_common.get_services_status())
+        kwargs.setdefault('telegrams', sql.get_user_telegram_by_group(user_group))
+        kwargs.setdefault('pds', sql.get_user_pd_by_group(user_group))
+        kwargs.setdefault('groups', sql.select_groups())
+        kwargs.setdefault('slacks', sql.get_user_slack_by_group(user_group))
+        kwargs.setdefault('haproxy_servers', roxywi_common.get_dick_permit(haproxy=1, only_group=1))
+        kwargs.setdefault('nginx_servers', roxywi_common.get_dick_permit(nginx=1, only_group=1))
+        kwargs.setdefault('apache_servers', roxywi_common.get_dick_permit(apache=1, only_group=1))
+        kwargs.setdefault('keepalived_servers', roxywi_common.get_dick_permit(keepalived=1, only_group=1))
+        kwargs.setdefault('user_subscription', user_subscription)
+        kwargs.setdefault('user_params', user_params)
+        kwargs.setdefault('lang', user_params['lang'])
+
+    return render_template('ajax/load_checker.html', **kwargs)
 
 
 def update_haproxy_settings(setting_id, email, service_alert, backend_alert, maxconn_alert, telegram_id, slack_id, pd_id) -> str:
