396d6b85e95498b4b0ff520e6421201625c95379
Aidaho <aidaho@roxy-wi.org>
2024-03-18 12:54:27 +0300

    v7.2.2.0
    Fix JS

--- inc/script.js
+++ inc/script.js
@@ -1680,3 +1680,14 @@ function removeData() {
         chart.destroy();
     }
 }
+function common_ajax_action_after_success(dialog_id, new_group, ajax_append_id, data) {
+	toastr.clear();
+	$("#"+ajax_append_id).append(data);
+	$( "."+new_group ).addClass( "update", 1000);
+	$.getScript(awesome);
+	clearTips();
+	$( dialog_id ).dialog("close" );
+	setTimeout(function() {
+		$( "."+new_group ).removeClass( "update" );
+	}, 2500 );
+}
--- inc/users.js
+++ inc/users.js
@@ -288,17 +288,6 @@ window.onload = function() {
 		}
 	}
 }
-function common_ajax_action_after_success(dialog_id, new_group, ajax_append_id, data) {
-	toastr.clear();
-	$("#"+ajax_append_id).append(data);
-	$( "."+new_group ).addClass( "update", 1000);
-	$.getScript(awesome);
-	clearTips();
-	$( dialog_id ).dialog("close" );
-	setTimeout(function() {
-		$( "."+new_group ).removeClass( "update" );
-	}, 2500 );
-}
 function addUser(dialog_id) {
 	var valid = true;
 	toastr.clear();
